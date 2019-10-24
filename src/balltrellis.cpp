07f8501ccacfcbb02f6f8cb0a5db55354fb0407c
Greg Tucker <greg.tucker@stfc.ac.uk>
2019-10-24 21:54:28 +0100

    Fixed divide-by-zero error for single-tetrahedra 'meshes'

--- src/balltrellis.cpp
+++ src/balltrellis.cpp
@@ -14,54 +14,58 @@
 */
 Trellis construct_trellis(const std::vector<TrellisLeaf>& leaves, const double fraction){
   std::array<double,3> p, c{0.,0.,0.};
-  std::array<double,9> xyz;
-  // find the centroid of the leaf positions
-  for (size_t i=0; i<3u; ++i) c[i] = 0.;
-  for (auto leaf: leaves){
-    p = leaf.centre();
-    for (size_t i=0; i<3u; ++i) c[i] += p[i];
-  }
-  for (size_t i=0; i<3u; ++i) c[i] /= static_cast<double>(leaves.size());
-  // plus the vector pointing to the farthest leaf from the centroid
-  // choose this direction to be our x-axis
-  double d, dmax = 0.;
-  for (auto leaf: leaves){
-    p = leaf.centre();
-    d = 0.;
-    for (size_t i=0; i<3u; ++i) d += (p[i]-c[i])*(p[i]-c[i]);
-    if (d>dmax){
-      dmax = d;
-      for (size_t i=0; i<3u; ++i) xyz[i] = p[i] - c[i];
+  std::array<double,9> xyz{1.,0.,0., 0.,1.,0., 0.,0.,1.};
+  double d;
+  // we need more than one leaf to be able to calculate x, y, and z
+  if (leaves.size()>1){
+    // find the centroid of the leaf positions
+    for (size_t i=0; i<3u; ++i) c[i] = 0.;
+    for (auto leaf: leaves){
+      p = leaf.centre();
+      for (size_t i=0; i<3u; ++i) c[i] += p[i];
     }
-  }
-  // normalise the x-axis:
-  d = 0.;
-  for (size_t i=0; i<3u; ++i) d += xyz[i]*xyz[i];
-  d = std::sqrt(d);
-  for (size_t i=0; i<3u; ++i) xyz[i] /= d;
-  // find the perpendicular direction with the farthest leaf from the centroid
-  // choose this to be our y-axis
-  dmax = 0.;
-  std::array<double,3> pperp;
-  for (auto leaf: leaves){
-    p = leaf.centre();
-    d = 0.; // p⋅x
-    for (size_t i=0; i<3u; ++i) d += p[i]*xyz[i]; // dot(p, ̂x)
-    for (size_t i=0; i<3u; ++i) pperp[i] = p[i] - d*xyz[i]; // p - ̂x dot(p, ̂x)
-    d = 0.;
-    for (size_t i=0; i<3u; ++i) d += pperp[i]*pperp[i];
-    if (d > dmax){
-      dmax = d;
-      for (size_t i=0; i<3u; ++i) xyz[i+3] = pperp[i];
+    for (size_t i=0; i<3u; ++i) c[i] /= static_cast<double>(leaves.size());
+    // plus the vector pointing to the farthest leaf from the centroid
+    // choose this direction to be our x-axis
+    double dmax = 0.;
+    for (auto leaf: leaves){
+      p = leaf.centre();
+      d = 0.;
+      for (size_t i=0; i<3u; ++i) d += (p[i]-c[i])*(p[i]-c[i]);
+      if (d>dmax){
+        dmax = d;
+        for (size_t i=0; i<3u; ++i) xyz[i] = p[i] - c[i];
+      }
     }
+    // normalise the x-axis:
+    d = 0.;
+    for (size_t i=0; i<3u; ++i) d += xyz[i]*xyz[i];
+    d = std::sqrt(d);
+    for (size_t i=0; i<3u; ++i) xyz[i] /= d;
+    // find the perpendicular direction with the farthest leaf from the centroid
+    // choose this to be our y-axis
+    dmax = 0.;
+    std::array<double,3> pperp;
+    for (auto leaf: leaves){
+      p = leaf.centre();
+      d = 0.; // p⋅x
+      for (size_t i=0; i<3u; ++i) d += p[i]*xyz[i]; // dot(p, ̂x)
+      for (size_t i=0; i<3u; ++i) pperp[i] = p[i] - d*xyz[i]; // p - ̂x dot(p, ̂x)
+      d = 0.;
+      for (size_t i=0; i<3u; ++i) d += pperp[i]*pperp[i];
+      if (d > dmax){
+        dmax = d;
+        for (size_t i=0; i<3u; ++i) xyz[i+3] = pperp[i];
+      }
+    }
+    // normalise the y-axis;
+    d = 0;
+    for (size_t i=0; i<3u; ++i) d += xyz[i+3]*xyz[i+3];
+    d = std::sqrt(d);
+    for (size_t i=0; i<3u; ++i) xyz[i+3] /= d;
+    // pick the z-axis as x × y
+    vector_cross(xyz.data()+6, xyz.data(), xyz.data()+3);
   }
-  // normalise the y-axis;
-  d = 0;
-  for (size_t i=0; i<3u; ++i) d += xyz[i+3]*xyz[i+3];
-  d = std::sqrt(d);
-  for (size_t i=0; i<3u; ++i) xyz[i+3] /= d;
-  // pick the z-axis as x × y
-  vector_cross(xyz.data()+6, xyz.data(), xyz.data()+3);
   // find the extents of the data
   std::array<std::array<double,2>,3> minmax;
   for (size_t i=0; i<3u; ++i){
@@ -82,13 +86,13 @@ Trellis construct_trellis(const std::vector<TrellisLeaf>& leaves, const double f
   // >>>>>>>>>>>>>>>>>>>>>>>>>>> optimisation testing
   double max_radius = 0.;
   for (auto leaf: leaves) if (leaf.radius() > max_radius) max_radius = leaf.radius();
-  info_update("The maximum leaf radius is ",max_radius);
-  info_update("And the leaves extend over");
-  info_update(" x = ",xyz[0]," ",xyz[1]," ",xyz[2]," : ",minmax[0]);
-  info_update(" y = ",xyz[3]," ",xyz[4]," ",xyz[5]," : ",minmax[1]);
-  info_update(" z = ",xyz[6]," ",xyz[7]," ",xyz[8]," : ",minmax[2]);
+  debug_update("The maximum leaf radius is ",max_radius);
+  debug_update("And the leaves extend over");
+  debug_update(" x = ",xyz[0]," ",xyz[1]," ",xyz[2]," : ",minmax[0]);
+  debug_update(" y = ",xyz[3]," ",xyz[4]," ",xyz[5]," : ",minmax[1]);
+  debug_update(" z = ",xyz[6]," ",xyz[7]," ",xyz[8]," : ",minmax[2]);
   d = max_radius * fraction;
-  info_update("Using a boundary step size of max_radius*",fraction," which is ",d);
+  debug_update("Using a boundary step size of max_radius*",fraction," which is ",d);
   // <<<<<<<<<<<<<<<<<<<<<<<<<<<
   // and construct the boundaries
   std::array<std::vector<double>,3> boundaries;
@@ -99,9 +103,11 @@ Trellis construct_trellis(const std::vector<TrellisLeaf>& leaves, const double f
     boundaries[i].push_back(minmax[i][0]);
     while( boundaries[i].back() < minmax[i][1] ) boundaries[i].push_back(boundaries[i].back()+d);
     // replace the first and last boundaries to -∞ and +∞, respectively
-    boundaries[i].front() = std::numeric_limits<double>::lowest();
-    boundaries[i].back() = (std::numeric_limits<double>::max)();
-    info_update("Step size along axis ",i,", ",d,", yields ",boundaries[i].size()-1," bins");
+    // boundaries[i].front() = std::numeric_limits<double>::lowest();
+    // boundaries[i].back() = (std::numeric_limits<double>::max)();
+    boundaries[i].front() = -std::numeric_limits<double>::infinity();
+    boundaries[i].back()  =  std::numeric_limits<double>::infinity();
+    debug_update("Step size along axis ",i,", ",d,", yields ",boundaries[i].size()-1," bins with boundaries ",boundaries[i]);
   }
 
   // Construct the Trellis object assigning the leaves to nodes
