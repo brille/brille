cd4c81cbb8f64417f57d39871a12e611fc5a5481
Greg Tucker <greg.tucker@stfc.ac.uk>
2020-07-29 16:49:51 +0100

    [Feature] Store built documentation in multiple repositories
    - With the new organisational account `brille` it seems like a good idea
      to make the `brille` library/module documentation available at the
      top-level GitHub Pages address https://brille.github.io/
      As a temporary measure it should continue to exist within the brille
      repository, now at https://brille.github.io/brille
    - Changing the Docker entrypoint.sh script to store the built
      documentation in multiple repositories only requires that their
      (relative) directory names be specified as a space-separated string
      which is then used in a for loop. As long as the ouput repositories
      are setup in advance by, e.g., actions/checkout@v2, the only change to
      the GitHub Actions workflow script is to specifiy all output
      repository directories in a single variable.
    - Eventually the in-repository generated documentation should be removed
      (and purged from the repository history). When that is done the
      extraneous checkout can be removed and the variable reduced to a
      single directory.

--- .github/actions/build_sphinx/entrypoint.sh
+++ .github/actions/build_sphinx/entrypoint.sh
@@ -9,7 +9,6 @@ function echo_run {
 }
 
 REPO_SRC="${GITHUB_WORKSPACE}/${INPUT_SOURCE_DIR}"
-GH_PAGES="${GITHUB_WORKSPACE}/${INPUT_PAGES_DIR}"
 SPHNX_DT=$GITHUB_WORKSPACE/.doctree
 
 echo ::group::Create working directories
@@ -37,9 +36,11 @@ echo_run python3 -m pip install $REPO_SRC
 echo ::endgroup::
 
 echo ::group::Configure pages author information
-echo_run cd $GH_PAGES
-echo_run git config user.name $AUTHOR_NAME
-echo_run git config user.email $AUTHOR_EMAIL
+for val in $INPUT_PAGES_DIR; do
+	echo_run cd "${GITHUB_WORKSPACE}/${val}"
+	echo_run git config user.name $AUTHOR_NAME
+	echo_run git config user.email $AUTHOR_EMAIL
+done
 echo ::endgroup::
 
 echo ::group::Build pages with Sphinx
@@ -60,24 +61,25 @@ if [ "${INPUT_CREATE_README}" = true ]; then
 	echo ::endgroup::
 fi
 
-echo ::group::Move documentation into the correct places
-named_dir="${GH_PAGES}/${named}"
-# Copy the newly built HTML pages into either gh_pages/latest or gh_pages/[version name]
-echo_run mkdir -p $named_dir
-echo_run rsync -a --delete "${HTML_DIR}/" $named_dir
-# make sure we're in the root of the gh-pages branch
-echo_run cd $GH_PAGES
-# for releases, move the stable symlink
-if [ "${INPUT_IS_RELEASE}" = true ]; then
-	echo_run unlink stable
-	echo_run ln -s ${named} stable
-fi
-# Add all changes to the gh_pages branch
-echo_run git add .
-echo ::endgroup::
-
-echo ::group::Commit to repository and push
-echo_run git commit --date="$(date)" --message='Auto commit from Versioned Sphinx GH-Pages Action'
-echo_run git push
-echo ::endgroup::
-
+for val in $INPUT_PAGES_DIR; do
+	echo ::group::Move documentation into the correct places
+	named_dir="{GITHUB_WORKSPACE}/${val}/${named}"
+	# Copy the newly built HTML pages into either gh_pages/latest or gh_pages/[version name]
+	echo_run mkdir -p $named_dir
+	echo_run rsync -a --delete "${HTML_DIR}/" $named_dir
+	# make sure we're in the root of the gh-pages branch
+	echo_run cd "${GITHUB_WORKSPACE}/${val}"
+	# for releases, move the stable symlink
+	if [ "${INPUT_IS_RELEASE}" = true ]; then
+		echo_run unlink stable
+		echo_run ln -s ${named} stable
+	fi
+	# Add all changes to the gh_pages branch
+	echo_run git add .
+	echo ::endgroup::
+	
+	echo ::group::Commit to repository and push
+	echo_run git commit --date="$(date)" --message='Auto commit from Versioned Sphinx GH-Pages Action'
+	echo_run git push
+	echo ::endgroup::
+done
