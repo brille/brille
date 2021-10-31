4d528a391eb13ee089189eae781183c46011103a
Gregory Tucker <gregory.tucker@gmail.com>
2021-10-31 09:27:39 +0100

    [Add] intersphinx config
    [Fix] broken equation display
    
    - Some equations rendered by katex did not display correctly. The
    	version of katex specified by sphinxcontrib.katex raised an error when
    	the character (or string?) '\r' was encountered in those equations.
    	Each of the effected equations includes the LaTeX command \AA --
    	whether any unaffected equations also include \AA has not been checked.
    - sphinxcontrib.katex allows specifying a different path for the
    	katex.css, katex.js, and auto-render.js files. The broken display has
    	been avoided by switching to the most-recent version of each v0.15.0,
    	which replaces the error with a warning message visible in the browser
    	console.
    - An alternative solution would have updated the version of
    	sphinxcontrib.katex in the Docker image, since the package now
    	specifies v0.13.11 of katex instead of v0.11.1. This may be a better
    	long-term solution but would require sorting-out the errors that
    	appear with newer version(s) of sphinx, breath, and/or exhale.

--- docs/conf.py
+++ docs/conf.py
@@ -42,6 +42,21 @@ extensions = [
     # 'exhale',
 ]
 
+intersphinx_mapping = {
+  'euphonic': ('https://euphonic.readthedocs.io/en/stable/', None),
+  'brilleu': ('https://brille.github.io/brilleu/latest/', None),
+}
+
+# Some :math:`[LaTeX]` directives insert '\r' into the string passed to katex?
+# This raises an error with the version specified in the Docker image's
+# sphinxcontrib.katex, 0.11.1, switching to v0.15.0 turns the error into
+# a warning about 'LaTeX-incompatible input' 
+# Updating the Docker image might be possible, but the exhale/breathe/sphinx
+# problem encountered outside of the Docker build would need to be avoided.
+katex_css_path = 'https://cdn.jsdelivr.net/npm/katex@0.15.0/dist/katex.min.css'
+katex_js_path = 'https://cdn.jsdelivr.net/npm/katex@0.15.0/dist/katex.min.js'
+katex_autorender_path = 'https://cdn.jsdelivr.net/npm/katex@0.15.0/dist/contrib/auto-render.min.js'
+
 napoleon_use_ivar = True
 napoleon_use_param = False
 napoleon_use_admonition_for_notes = True
