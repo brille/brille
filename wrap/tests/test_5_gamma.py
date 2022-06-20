992e8cba7c043c45aa916427a2b57f8b059888bd
Gregory Tucker <gregory.tucker@ess.eu>
2022-06-20 11:13:51 +0200

    [Refactor] Python tests' module loading

--- wrap/tests/test_5_gamma.py
+++ wrap/tests/test_5_gamma.py
@@ -1,26 +1,10 @@
 #!/usr/bin/env python3
-import os
-import sys
 import unittest
 import numpy as np
 from pathlib import Path
-from importlib.util import find_spec
-addpaths = [Path(), Path('..')] # we might be in .../build/wrap/
-config = os.environ.get('CMAKE_CONFIG_TYPE') # set by ctest -C <cfg>
-if config:
-  for path in addpaths:
-    if Path(path, config).exists():
-      addpaths.append(Path(path,config))
-sys.path[:0] = [str(path.absolute()) for path in addpaths]
 
-if find_spec('_brille') is not None:
-    import _brille as s
-elif find_spec('brille') is not None and find_spec('brille._brille') is not None:
-    import brille as s
-else:
-    abspaths = [str(path.absolute()) for path in addpaths]
-    raise Exception("brille module not found in {}!".format(abspaths))
-
+from load_local import load
+s = load(('_brille', 'brille._brille'), prefer_installed=True, search=[Path(), Path('..')])
 
 def fetchLoad(loader, fetchfile, **kwds):
     """Fetch remote binary file from the :py:mod:`brille` repository
