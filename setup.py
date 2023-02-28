5688d5f635f33c17e68bd97812c64a96862fa9bb
Gregory Tucker <gregory.tucker@ess.eu>
2023-02-28 14:58:15 +0100

    [Add] Numpy hard dependency, cleanup pyproject.toml

--- setup.py
+++ setup.py
@@ -133,6 +133,7 @@ setup(
     long_description_content_type="text/markdown",
     ext_modules=[CMakeExtension('brille._brille')],
     packages=find_packages(str(PACKAGE_ROOT)),
+    install_requires=['numpy'],
     extras_require={'plotting': ['matplotlib>=2.2.0', ], 'vis': ['pyglet>=1.5.27', 'vispy>=0.12.1', ]},
     cmdclass=dict(build_ext=CMakeBuild),
     url="https://github.com/brille/brille",
